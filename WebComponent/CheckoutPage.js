28284bcdda35fff96b5e7f69ce9eaa58cc5c9b98
WahidFirgiyanto <firgiyantowahid@gmail.com>
2024-12-28 22:34:51 +0700

    Memperbaiki coment

--- WebComponent/CheckoutPage.js
+++ WebComponent/CheckoutPage.js
@@ -1,31 +1,27 @@
-const { By } = require('selenium-webdriver'); // Impor By
-const { WebDriver } = require('selenium-webdriver'); // Jika perlu
+const { By } = require('selenium-webdriver');
+const { WebDriver } = require('selenium-webdriver');
 
 class CheckoutPage {
     constructor(driver) {
         this.driver = driver;
     }
 
-    // Get the current page title (useful for assertions)
     async getCheckoutTitle() {
         const titleElement = await this.driver.findElement(By.className('title'));
         return await titleElement.getText();
     }
 
-    // Fill out the checkout form
     async fillCheckoutForm(firstName, lastName, postalCode) {
         await this.driver.findElement(By.id('first-name')).sendKeys(firstName);
         await this.driver.findElement(By.id('last-name')).sendKeys(lastName);
         await this.driver.findElement(By.id('postal-code')).sendKeys(postalCode);
     }
 
-    // Submit the checkout form
     async submitCheckoutForm() {
         const continueButton = await this.driver.findElement(By.id('continue'));
         await continueButton.click();
     }
 
-    // Click the Finish button to complete the checkout
     async clickFinishButton() {
         const finishButton = await this.driver.findElement(By.id('finish'));
         await finishButton.click();
--- WebComponent/DashboardPage.js
+++ WebComponent/DashboardPage.js
@@ -5,7 +5,6 @@ class DashboardPage {
         this.driver = driver;
     }
 
-    // Menavigasi ke halaman Dashboard
     async navigate() {
         await this.driver.get('https://www.saucedemo.com/');
     }
@@ -17,27 +16,22 @@ class DashboardPage {
         await this.driver.findElement(By.id('login-button')).click();
     }
 
-    // Memeriksa apakah kita berada di halaman dashboard
     async isOnDashboard() {
         const title = await this.driver.getTitle();
         return title;
     }
 
-    // Menambahkan item pertama ke keranjang
     async addFirstItemToCart() {
         await this.driver.findElement(By.css('.inventory_item:nth-child(1) .btn_inventory')).click();
     }
 
-    // Mendapatkan jumlah badge cart
     async getCartBadgeCount() {
         const badge = await this.driver.findElement(By.css('.shopping_cart_badge'));
         const badgeText = await badge.getText();
         return parseInt(badgeText, 10);
     }
 
-    // Menavigasi ke halaman Cart
     async navigateToCart() {
-        // Klik ikon cart untuk pergi ke halaman cart
         const cartIcon = await this.driver.findElement(By.css('.shopping_cart_link'));
         await cartIcon.click();
     }
